80e90442281534244b9129f24669f0a102757dbe
VavakinV <vladvavakin@gmail.com>
2024-09-13 20:35:59 +0300

    Задача 4. Метод 3.
    Метод поиска НОД максимального нечетного непростого делителя числа и произведения всех его цифр.
    
    Вспомогательный методы для нечетного непростого делителя и произведения цифр.
    
    Небольшие корректировки кода и вывода проверки

--- Лаб1/4)DigitalMethods.rb
+++ Лаб1/4)DigitalMethods.rb
@@ -30,6 +30,23 @@ def max_prime_divisor(n)
 end
 
 
+# Произведение всех цифр числа
+def digits_mult(n)
+	# Искомое произведение
+	result = 1
+
+	# С каждой итерацией получаем последнюю цифру
+	# и отбрасываем её целочисленным делением на 10
+	while n >= 1
+		last_digit = n % 10
+		result *= last_digit 
+		n /= 10
+	end
+
+	return result
+end  
+
+
 # Произведение всех цифр числа, не делящихся на 5
 def not5_digits_mult(n)
 	# Искомое произведение
@@ -40,7 +57,7 @@ def not5_digits_mult(n)
 	while n >= 1
 		last_digit = n % 10
 		if last_digit % 5 != 0
-			result *= n % 10 
+			result *= last_digit
 		end
 		n /= 10
 	end
@@ -49,8 +66,34 @@ def not5_digits_mult(n)
 end  
 
 
+# Поиск максимального непростого нечетного делителя
+def max_odd_not_prime_divisor(n)
+	result = 1
+
+	# Поиск наибольшего среди чисел от 2 до n,
+	# которые являются делителями и нечетными непростыми числами
+	(2..n).each do |i|
+		if n % i == 0 && !is_prime(i) && i % 2 == 1
+			result = i
+		end
+	end
+
+	return result
+end
+
+
+# НОД максимального нечетного непростого делителя и произведения цифр
+def div_mult_gcd(n)
+	monpd = max_odd_not_prime_divisor(n)
+	dm = digits_mult(n)
+	return monpd.gcd(dm)
+end
+
 # Проверка работы методов
 print "Введите число: "
 number = $stdin.gets.chomp.to_i
-puts "Максимальный простой делитель #{number}: #{max_prime_divisor(number)}"
-puts "Произведение цифр в #{number}, не делящихся на 5: #{not5_digits_mult(number)}"
+puts "Метод 1)Максимальный простой делитель #{number}: #{max_prime_divisor(number)}"
+puts "Метод 2)Произведение цифр в #{number}, не делящихся на 5: #{not5_digits_mult(number)}"
+puts "Максимальный нечетный непростой делитель: #{max_odd_not_prime_divisor(number)}"
+puts "Произведение всех цифр #{number}: #{digits_mult(number)}"
+puts "Метод 3)НОД максимального нечетного непростого делителя и произведения цифр #{number}: #{div_mult_gcd(number)}"
