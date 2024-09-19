f08f40f41d6f153a3b6618212a5c91a62e72f260
VavakinV <vladvavakin@gmail.com>
2024-09-20 00:13:18 +0300

    Задача 8
    Методы валидации корректной формы строки для остальных полей. Изменены геттеры, сеттеры и конструктор.
    
    Изменены данные для проверки.

--- Лаб2/Student.rb
+++ Лаб2/Student.rb
@@ -1,22 +1,45 @@
 class Student
 	# Конструктор класса
 	def initialize(surname, firstname, lastname, **params)
-		@surname = surname
-		@firstname = firstname
-		@lastname = lastname
-		@id = params[:id]
+		self.surname = surname
+		self.firstname = firstname
+		self.lastname = lastname
+		self.id = params[:id]
 		self.phone_number = params[:phone_number]
-		@telegram = params[:telegram]
-		@email = params[:email]
-		@git = params[:git]
+		self.telegram = params[:telegram]
+		self.email = params[:email]
+		self.git = params[:git]
 	end
 
-	# Геттеры и сеттеры полей
+	# Геттеры для полей
+	attr_reader :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
 
-	attr_accessor :surname, :firstname, :lastname, :id, :telegram, :email, :git
+	# Сеттер для фамилии
+	def surname=(val)
+		if self.class.valid_name?(val)
+			@surname = val
+		else
+			raise ArgumentError, "Некорректная фамилия"
+		end
+	end
 
-	# Геттер для номера телефона
-	attr_reader :phone_number
+	# Сеттер для имени
+	def firstname=(val)
+		if self.class.valid_name?(val)
+			@firstname = val
+		else
+			raise ArgumentError, "Некорректное имя"
+		end
+	end
+
+	# Сеттер для отчества
+	def lastname=(val)
+		if self.class.valid_name?(val)
+			@lastname = val
+		else
+			raise ArgumentError, "Некорректное отчество"
+		end
+	end
 
 	# Сеттер для номера телефона
 	def phone_number=(val)
@@ -27,13 +50,74 @@ class Student
 		end
 	end
 
-	# Вывод информации о студенте
-	def print_info
-		puts "------------------\n" + "Студент: #{@surname} #{@firstname} #{@lastname}" + (@id ? "\nID: #{@id}" : "") + (@phone_number ? "\nНомер телефона: #{@phone_number}" : "") + (@telegram ? "\nTelegram: #{@telegram}" : "") + (@email ? "\ne-mail: #{@email}" : "") + (@git ? "\nGit: #{@git}" : "") + "\n------------------"
+	# Сеттер для ID
+	def id=(val)
+		if self.class.valid_id?(val)
+			@id = val 
+		else
+			puts "#{surname} #{firstname} #{lastname}: Некорректный ID"
+		end
+	end
+
+	# Сеттер для Telegram
+	def telegram=(val)
+		if self.class.valid_telegram?(val)
+			@telegram = val 
+		else
+			puts "#{surname} #{firstname} #{lastname}: Некорректный telegram"
+		end
+	end
+
+	# Сеттер для email
+	def email=(val)
+		if self.class.valid_email?(val)
+			@email = val 
+		else
+			puts "#{surname} #{firstname} #{lastname}: Некорректный email"
+		end
+	end
+
+	# Сеттер для Git
+	def git=(val)
+		if self.class.valid_git?(val)
+			@git = val 
+		else
+			puts "#{surname} #{firstname} #{lastname}: Некорректный git"
+		end
 	end
 
 	# Метод валидации номера телефона
 	def self.valid_phone_num?(str)
 		str.nil? || str.match?(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/)
+	end
+
+	# Метод валидации ФИО
+	def self.valid_name?(str)
+		str.match?(/^[A-ZА-Я][a-zA-Zа-яА-Я\-]{0,49}$/)
+	end
+
+	# Метод валидации ID
+	def self.valid_id?(str)
+		str.nil? || str.class == Integer ? true : str.match?(/^(\d+)$/) ? true : false
+	end
+
+	# Метод валидации Telegram
+	def self.valid_telegram?(str)
+		str.nil? || str.match?(/^(?:@|(?:(?:(?:https?:\/\/)?t(?:elegram)?)\.me\/))(\w{4,})$/)
+	end
+
+	# Метод валидации email
+	def self.valid_email?(str)
+		str.nil? || str.match?(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)
+	end
+
+	# Метод валидации Git
+	def self.valid_git?(str)
+		str.nil? || str.match?(/^(?:https:\/\/github\.com\/)?[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/)
+	end
+
+	# Вывод информации о студенте
+	def print_info
+		puts "------------------\n" + "Студент: #{@surname} #{@firstname} #{@lastname}" + (@id ? "\nID: #{@id}" : "") + (@phone_number ? "\nНомер телефона: #{@phone_number}" : "") + (@telegram ? "\nTelegram: #{@telegram}" : "") + (@email ? "\ne-mail: #{@email}" : "") + (@git ? "\nGit: #{@git}" : "") + "\n------------------"
 	end
 end
--- Лаб2/main.rb
+++ Лаб2/main.rb
@@ -1,15 +1,11 @@
 require "./Student.rb"
 
-# ivan = Student.new("Иванов", "Иван", "Иванович", id: 1, phone_number: "88005553535", git: "https://github.com/Ivan")
-# nikita = Student.new("Козлов", "Никита", "Борисович", id: 2, phone_number: "89181112233", telegram: "Sebas", git: "https://github.com/SebaSxc")
-# masha = Student.new("Ульянова", "Мария", "Сергеевна", id: 3, phone_number: "89889998877", telegram:"Flower")
-
-pacman = Student.new("Пак", "Ман", "Лол", phone_number: "0")
+ivan = Student.new("Иванов", "Иван", "Иванович", id: 1, phone_number: "88005553535", git: "https://github.com/ivan")
+nikita = Student.new("Козлов", "Никита", "Борисович", id: 2, phone_number: "89181112233", telegram: "@Sebas", git: "sebasxc")
+masha = Student.new("Ульянова", "Мария", "Сергеевна", id: 3, phone_number: "89889998877", telegram: "t.me/Flower")
 sergey = Student.new("Стрельцов", "Сергей", "Владимирович", phone_number: "89182297016")
 
-# ivan.print_info
-# nikita.print_info
-# masha.print_info
-
-pacman.print_info
+ivan.print_info
+nikita.print_info
+masha.print_info
 sergey.print_info
