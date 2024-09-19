9b7614a1a7cfb4fb4011170396db57bd27ec24ab
VavakinV <vladvavakin@gmail.com>
2024-09-19 22:43:50 +0300

    Задача 7
    Валидация номера телефона. Модифицирован конструктор, геттер и сеттер для номера телефона. Изменён файл main для проверки работоспособности метода.

--- Лаб2/Student.rb
+++ Лаб2/Student.rb
@@ -5,7 +5,7 @@ class Student
 		@firstname = firstname
 		@lastname = lastname
 		@id = params[:id]
-		@phone_number = params[:phone_number]
+		self.phone_number = params[:phone_number]
 		@telegram = params[:telegram]
 		@email = params[:email]
 		@git = params[:git]
@@ -13,10 +13,27 @@ class Student
 
 	# Геттеры и сеттеры полей
 
-	attr_accessor :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
+	attr_accessor :surname, :firstname, :lastname, :id, :telegram, :email, :git
+
+	# Геттер для номера телефона
+	attr_reader :phone_number
+
+	# Сеттер для номера телефона
+	def phone_number=(val)
+		if self.class.valid_phone_num?(val)
+			@phone_number = val
+		else 
+			puts "#{surname} #{firstname} #{lastname}: Некорректный номер телефона"
+		end
+	end
 
 	# Вывод информации о студенте
 	def print_info
 		puts "------------------\n" + "Студент: #{@surname} #{@firstname} #{@lastname}" + (@id ? "\nID: #{@id}" : "") + (@phone_number ? "\nНомер телефона: #{@phone_number}" : "") + (@telegram ? "\nTelegram: #{@telegram}" : "") + (@email ? "\ne-mail: #{@email}" : "") + (@git ? "\nGit: #{@git}" : "") + "\n------------------"
+	end
+
+	# Метод валидации номера телефона
+	def self.valid_phone_num?(str)
+		str.nil? || str.match?(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/)
 	end
 end
--- Лаб2/main.rb
+++ Лаб2/main.rb
@@ -1,7 +1,15 @@
 require "./Student.rb"
 
-ivan = Student.new("Иванов", "Иван", "Иванович", id: 1, phone_number: "88005553535", git: "https://github.com/Ivan")
-# nikita = Student.new("Козлов", "Никита", "Борисович", 2, "89181112233", "Sebas", "https://github.com/SebaSxc")
-# masha = Student.new("Ульянова", "Мария", "Сергеевна", 3, "89889998877", "Flower")
+# ivan = Student.new("Иванов", "Иван", "Иванович", id: 1, phone_number: "88005553535", git: "https://github.com/Ivan")
+# nikita = Student.new("Козлов", "Никита", "Борисович", id: 2, phone_number: "89181112233", telegram: "Sebas", git: "https://github.com/SebaSxc")
+# masha = Student.new("Ульянова", "Мария", "Сергеевна", id: 3, phone_number: "89889998877", telegram:"Flower")
 
-ivan.print_info
+pacman = Student.new("Пак", "Ман", "Лол", phone_number: "0")
+sergey = Student.new("Стрельцов", "Сергей", "Владимирович", phone_number: "89182297016")
+
+# ivan.print_info
+# nikita.print_info
+# masha.print_info
+
+pacman.print_info
+sergey.print_info
