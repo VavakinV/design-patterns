2aed9ee47c30a0961f995db011f5ca4e93bf7eb5
VavakinV <vladvavakin@gmail.com>
2024-09-20 23:30:11 +0300

    Задача 5
    Суперкласс Person

--- Kernel Classes and Models (Lab 2)/Student.rb
+++ Kernel Classes and Models (Lab 2)/Student.rb
@@ -1,4 +1,40 @@
-class Student
+class Person
+	attr_reader :id, :git
+
+	protected
+
+	# Метод валидации номера телефона
+	def self.valid_phone_number?(str)
+		str.nil? || str.match?(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/)
+	end
+
+	# Метод валидации ФИО
+	def self.valid_name?(str)
+		str.match?(/^[A-ZА-Я][a-zA-Zа-яА-Я\-]{0,49}$/)
+	end
+
+	# Метод валидации ID
+	def self.valid_id?(str)
+		str.nil? || str.class == Integer
+	end
+
+	# Метод валидации Telegram
+	def self.valid_telegram?(str)
+		str.nil? || str.match?(/^(?:@|(?:(?:(?:https?:\/\/)?t(?:elegram)?)\.me\/))(\w{4,})$/)
+	end
+
+	# Метод валидации email
+	def self.valid_email?(str)
+		str.nil? || str.match?(/^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/)
+	end
+
+	# Метод валидации Git
+	def self.valid_git?(str)
+		str.nil? || str.match?(/^(?:https:\/\/github\.com\/)?[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/)
+	end
+end
+
+class Student < Person
 	# Конструктор класса
 	def initialize(surname, firstname, lastname, **params)
 		self.surname = surname
@@ -10,7 +46,7 @@ class Student
 	end
 
 	# Геттеры для полей
-	attr_reader :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
+	attr_reader :surname, :firstname, :lastname, :phone_number, :telegram, :email
 
 	# Сеттер для фамилии
 	def surname=(val)
@@ -65,36 +101,6 @@ class Student
 		else
 			raise ArgumentError, "Некорректный git"
 		end
-	end
-
-	# Метод валидации номера телефона
-	def self.valid_phone_number?(str)
-		str.nil? || str.match?(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/)
-	end
-
-	# Метод валидации ФИО
-	def self.valid_name?(str)
-		str.match?(/^[A-ZА-Я][a-zA-Zа-яА-Я\-]{0,49}$/)
-	end
-
-	# Метод валидации ID
-	def self.valid_id?(str)
-		str.nil? || str.class == Integer
-	end
-
-	# Метод валидации Telegram
-	def self.valid_telegram?(str)
-		str.nil? || str.match?(/^(?:@|(?:(?:(?:https?:\/\/)?t(?:elegram)?)\.me\/))(\w{4,})$/)
-	end
-
-	# Метод валидации email
-	def self.valid_email?(str)
-		str.nil? || str.match?(/^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/)
-	end
-
-	# Метод валидации Git
-	def self.valid_git?(str)
-		str.nil? || str.match?(/^(?:https:\/\/github\.com\/)?[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/)
 	end
 
 	# Вывод информации о студенте
@@ -153,8 +159,8 @@ class Student
 	end
 end
 
-class Student_short
-	attr_reader :id, :initials, :git, :contacts
+class Student_short < Person
+	attr_reader :initials, :contacts
 
 	# Конструктор принимает хэш с либо полем student:, либо двумя полями id: и contacts:
 	def initialize(params)
@@ -232,5 +238,3 @@ class Student_short
 		end
 	end
 end
-
-
