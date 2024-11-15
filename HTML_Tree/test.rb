c4fbe441b469e596d61446bdab045be74fde297a
VavakinV <vladvavakin@gmail.com>
2024-11-15 20:47:54 +0300

    Лаб 3. Задание 3. Корректировки.
    Дополнен вывод в файле тестирования.

--- HTML_Tree/test.rb
+++ HTML_Tree/test.rb
@@ -1,12 +1,23 @@
 require "./html_tree.rb"
 
-html = "<html><body><div class=\"container\"><p>Hello, world!</p></div><div></div></body></html>"
+html = "
+<html>
+	<body>
+		<div class=\"container\">
+			<p>Hello, world!</p>
+		</div>
+		<div>
+		</div>
+	</body>
+</html>"
+
 tree = HtmlTree.new(html)
-puts "Метод count для дерева: #{tree.count}"
-puts "Метод count с условием (имя div): #{tree.count{|x| x.name == "div"}}"
-puts "Количество детей у первого наследника корня: #{tree.root.children[0].count_children}"
-puts "Вывод экземпляра дерева в формате строки: #{tree.root}"
-puts "Вывод всех элементов дерева с помощью обхода в глубину: "
+puts "\nМетод count для дерева: #{tree.count}"
+puts "\nМетод find для дерева (условие - имя 'body'): #{tree.find{|x| x.name = "body"}.to_s}"
+puts "\nМетод count с условием (имя div): #{tree.count{|x| x.name == "div"}}"
+puts "\nКоличество детей у первого наследника корня: #{tree.root.children[0].count_children}"
+puts "\nВывод экземпляра дерева в формате строки: #{tree.root}"
+puts "\nВывод всех элементов дерева с помощью обхода в глубину: "
 tree.each{|node| puts node}
-puts "Метод has_children для всех элементов дерева: "
+puts "\nМетод has_children для всех элементов дерева: "
 tree.each{|node| puts "#{node.name}: #{node.has_children?}"}
