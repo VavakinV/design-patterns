cbacaa03f04ee5a73b767d78b8389e86c00586b4
VavakinV <vladvavakin@gmail.com>
2024-11-28 23:51:02 +0300

    Лаб 4. Задание 1. Задача 4.
    Реализация методов select, get_selected, get_names, get_data.

--- Student/models/data_list.rb
+++ Student/models/data_list.rb
@@ -2,11 +2,36 @@ class Data_List
 
     def initialize(data)
         self.data = data
+        self.selected = []
+    end
+
+    # Выбор элемента по номеру
+    def select(number)
+        unless number.is_a?(Integer) && number >= 0 && number < data.size
+            raise ArgumentError, 'Параметр должен быть числом в пределах размера массива'
+        end
+        self.selected << number unless self.selected.include?(number)
+    end
+
+    # Получение ID выбранных элементов
+    def get_selected
+        self.selected
+    end
+
+    # Получение массива наименований атрибутов
+    def get_names
+        raise NotImplementedError, 'Метод не реализован в данном классе'
+    end
+
+    # Получение объекта класса Data_Table
+    def get_data
+        raise NotImplementedError, 'Метод не реализован в данном классе'
     end
 
     private
 
     attr_reader :data
+    attr_accessor :selected
 
     # Сеттер для поля
     def data=(data)
